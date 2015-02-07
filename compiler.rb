4814e15d36f9f329673788ea643df3efd83b07fb
Jesus Riles <jesus.gm19@hotmail.com>
2015-02-07 00:03:53 -0600

    update4

--- compiler.rb
+++ compiler.rb
@@ -3,8 +3,9 @@ require 'fileutils'
 class Lexico
 
 	# class variables
-	@@ReservWords = ['suma',	'resta',	'multiplica',	'divide',	'guardalo',
-										'mas',	'menos',	'por',				'entre',	'en'].freeze
+	@@ReservWords = ['suma',	'resta',	'multiplica',	'divide',	'guardalo',		'definir',	'dejalo',
+										'mas',	'menos',	'por',				'entre',	'en',					'como'].freeze
+
 
 	def readFile(fileName = "code.txt")
 	'''
@@ -15,11 +16,11 @@ class Lexico
 		@words = Array.new()
 		file = File.open(@fileName, "r") # "r" stands for read
 
-		# split the content of the file by words
+		counter = 0
 		file.each_line do |line|
 			for word in line.split(" ")
-				@words = word
-				puts(@words)
+				@words[counter] = word
+				counter += 1
 			end
 		end
 
@@ -31,13 +32,48 @@ class Lexico
 	def classifyWord(word)
 		'''
 			Return the classification of the word.
+
+			Example on C:
+				int (keyword), value (identifier), = (operator), 100 (constant) and ; (symbol).
 		'''
 		@word = word
+		@Classif = ['keyword', 				# 0
+								'identifier', 		# 1
+								'operator', 			# 2
+								'constant', 			# 3
+								'symbol'].freeze	# 4
+
+		# check if @word start with a letter
+		if(@word[0] =~ /[A-Z]/ || @word[0] =~ /[a-z]/)
+			if(@@ReservWords.include?(@word))
+				return @Classif[0]	# keyword
+			end
+				return @Classif[1]	# identifier
+		end
 
 	end # end function
 
 	# access controls
-	public	:classifyWord
-	private	:readFile
+	public	:classifyWord, :readFile
 
 end # end class
+
+
+'''
+	Start - Testing Area
+'''
+test = Lexico.new()
+
+# readFile()
+fileContent = Array.new()
+
+fileContent = test.readFile()
+
+# classifyWord()
+for word in fileContent
+	puts(word + "\t\tcategory:\t\t" + test.classifyWord(word).to_s)
+end
+
+'''
+	Ends - Testing Area
+'''
