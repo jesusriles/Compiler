5fa7f3bf5beab06cbfcf6d0a948e9680238f6ed8
Jesus Riles <jesus.gm19@hotmail.com>
2015-02-07 20:16:33 -0600

    this close #1

--- compiler.rb
+++ compiler.rb
@@ -19,6 +19,13 @@ class Lexical
 		counter = 0
 		file.each_line do |line|
 			for word in line.split(" ")
+
+				if(word.include?(','))
+					@words[counter] = ","
+					counter += 1
+					word.delete!(',')
+				end
+
 				@words[counter] = word
 				counter += 1
 			end
@@ -32,6 +39,7 @@ class Lexical
 	def classifyWord(word)
 		'''
 			Return the classification of the word.
+			Note: Identifiers can only contain lower letters.
 
 			Example on C:
 				int (keyword), value (identifier), = (operator), 100 (constant) and ; (symbol).
@@ -39,15 +47,37 @@ class Lexical
 		@word = word
 		@Classif = ['keyword', 				# 0
 								'identifier', 		# 1
-								'operator', 			# 2
-								'constant', 			# 3
-								'symbol'].freeze	# 4
+								'constant', 			# 2
+								'symbol'].freeze	# 3
 
-		if(@word[0] =~ /[A-Z]/ || @word[0] =~ /[a-z]/)
+		if(@word[0] =~ /[a-z]/)
 			if(@@ReservWords.include?(@word))
 				return @Classif[0]	# keyword
-			end
+			else
+					@word.split("").each do |letter|
+						if(!(letter =~ /[a-z]/))
+							Kernel.abort("Error syntaxis on: '" + @word + "'")
+						end
+					end
 				return @Classif[1]	# identifier
+			end	
+
+		elsif(@word[0] =~ /[0-9]/)
+			dot = 0
+			@word.split("").each do |letter|
+				if(letter == '.')
+					dot += 1
+				end
+				if(!(letter =~ /[0-9]/) && (dot == 2))
+					Kernel.abort("Error syntaxis on: '" + @word + "'")
+				end
+			end
+			return @Classif[2]	# constant
+			
+		elsif(@word[0] == ',')
+			return @Classif[3]	# symbol
+		else
+			Kernel.abort("Error syntaxis on: '" + @word + "'")
 		end
 
 	end # end function
